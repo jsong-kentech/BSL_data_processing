6d3b07b2a7508a2f6b7c1ac87e993d90d90ce77e
jsong-kentech <jsong@kentech.ac.kr>
2023-04-19 18:54:38 +0900

    1

--- BSL_Postprocess_OCV.m
+++ BSL_Postprocess_OCV.m
@@ -5,8 +5,9 @@ clc; clear; close all;
 %% Interface
 
 % data folder
-data_folder = 'C:\Users\jsong\Documents\MATLAB\Data\OCP\OCP0.05C_Full cell(half)(5)';
-% data_folder = 'C:\Users\jsong\Documents\MATLAB\Data\OCP\OCP0.01C_Cathode Half cell(5)';
+% data_folder = 'C:\Users\jsong\Documents\MATLAB\Data\OCP\OCP0.05C_Full cell(half)(5)';
+ data_folder = 'C:\Users\jsong\Documents\MATLAB\Data\OCP\OCP0.01C_Cathode Half cell(5)';
+ [save_folder,save_name] = fileparts(data_folder);
 
 % cathode, fullcell, or anode
 id_cfa = 1; % 1 for cathode, 2 for fullcell, 3 for anode, 0 for automatic (not yet implemented)
@@ -18,12 +19,7 @@ step_ocv_dis = 6;
 
 % parameters
 y1 = 0.215685; % cathode stoic at soc = 100%. reference: AVL NMC811
-
-
-save_path = data_folder;
-sample_plot =2;
-
-
+x_golden=0.5; %
 
 
 
@@ -50,29 +46,69 @@ for i = 1:length(files)
    if id_cfa == 1 % cathode
         data(step_ocv_chg).stoic = 1-(1-y1)*data(step_ocv_chg).soc;
         data(step_ocv_dis).stoic = 1-(1-y1)*data(step_ocv_dis).soc; 
-   elseif id_cfa ==2 % anode
+   elseif id_cfa ==3 % anode
         data(step_ocv_chg).stoic = data(step_ocv_chg).soc;
         data(step_ocv_dis).stoic = data(step_ocv_dis).soc; 
+   elseif id_cfa == 2 % full cell
+       % stoic is not defined for full cell.
    end
 
 
+   % make an overall OCV struct
+   if id_cfa == 1 || id_cfa == 3 % cathode or anode halfcell
+        x_chg = data(step_ocv_chg).stoic;
+        y_chg = data(step_ocv_chg).V;
+        x_dis = data(step_ocv_dis).stoic;
+        y_dis = data(step_ocv_dis).V;
+   elseif id_cfa == 3 % fullcell
+        x_chg = data(step_ocv_chg).soc;
+        y_chg = data(step_ocv_chg).V;
+        x_dis = data(step_ocv_dis).soc;
+        y_dis = data(step_ocv_dis).V;
+
+   end
+    OCV_all(i).OCVchg = [x_chg y_chg];
+    OCV_all(i).OCVdis = [x_dis y_dis];
+
+    OCV_all(i).Qchg = data(step_ocv_chg).Q;
+    OCV_all(i).Qdis = data(step_ocv_dis).Q;
+
+    % golden criteria
+    OCV_all(i).y_golden = (interp1(x_chg,y_chg,0.5)+interp1(x_dis,y_dis,0.5))/2;
+    
+
     % plot
     color_mat=lines(3);
+    if i ==1
     figure
+    end
     hold on; box on;
-    plot(data(step_ocv_chg).soc,data(step_ocv_chg).V,'-',"Color",color_mat(1,:))
-    plot(data(step_ocv_dis).soc,data(step_ocv_dis).V,'-',"Color",color_mat(2,:))
+    plot(x_chg,y_chg,'-',"Color",color_mat(1,:))
+    plot(x_dis,y_dis,'-',"Color",color_mat(2,:))
     %axis([0 1 3 4.2])
     xlim([0 1])
     set(gca,'FontSize',12)
 
-    % capacity matrix
-    capacity_chg(i,1) = data(step_ocv_chg).Q;
-    capacity_dis(i,1) = data(step_ocv_dis).Q;
 
-
-   % make an overall OCV struct
     
-   % save the OCV struc
-
 end
+
+
+
+% select an golden OCV
+[~,i_golden] = min(abs([OCV_all.y_golden]-median([OCV_all.y_golden])));
+OCV_golden.i_golden = i_golden;
+
+% save OCV struct
+OCV_golden.OCVchg = OCV_all(1,i_golden).OCVchg; 
+OCV_golden.OCVdis = OCV_all(1,i_golden).OCVdis;
+
+
+% plot
+plot(OCV_golden.OCVchg(:,1),OCV_golden.OCVchg(:,2),'--','color',color_mat(3,:))
+plot(OCV_golden.OCVdis(:,1),OCV_golden.OCVdis(:,2),'--','color',color_mat(3,:))
+
+
+% save
+save_fullpath = [save_folder filesep save_name '.mat'];
+save(save_fullpath,'OCV_golden','OCV_all')
